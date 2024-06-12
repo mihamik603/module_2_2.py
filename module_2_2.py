c60cae04a652d43c6a4659ca45e3d82de867feb4
mihamik603 <vmm1331@gmail.com>
2024-06-12 21:29:57 +0400

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,27 @@
+first = 458
+second = 815
+third = 112
+if first == second and first == third and third == second:
+    print(3)
+elif first == second or first == third or third == second:
+    print(2)
+else:
+    print(0)
+first = 55
+second = 24
+third = 55
+if first == second and first == third and third == second:
+    print(3)
+elif first == second or first == third or third == second:
+    print(2)
+else:
+    print(0)
+first = 10
+second = 10
+third = 10
+if first == second and first == third and third == second:
+    print(3)
+elif first == second or first == third or third == second:
+    print(2)
+else:
+    print(0)
